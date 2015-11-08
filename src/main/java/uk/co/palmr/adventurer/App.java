34fff3486fa0bf5a09e030a37567326127ff66ff
Nick Palmer <nick@palmr.co.uk>
2015-11-08 03:14:39 +0000

    Mostly reformatting, logging, and adding word count property to pages

--- src/main/java/uk/co/palmr/adventurer/App.java
+++ src/main/java/uk/co/palmr/adventurer/App.java
@@ -7,6 +7,8 @@ import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfObject;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Label;
@@ -24,6 +26,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class App {
+  private static final Logger LOGGER = LogManager.getLogger(App.class);
+
   /**
    * Pages can either continue onto the next page or be linked to another page via a choice
    */
@@ -43,145 +47,222 @@ public class App {
     Ignore
   }
 
-  // Property names used on nodes to help navigate pages
+  // Property names for data stored on nodes
   private static final String PDF_PAGE_NUMBER = "pdf_page_number";
   private static final String BOOK_PAGE_LABEL = "book_page_label";
+  private static final String WORD_COUNT = "word_count";
 
   public static void main(String[] args) throws IOException {
     String pdfFilePath = System.getProperty("user.dir") + File.separator + "resources" + File.separator + "tbontb-regular.pdf";
-    String graphDbPath = System.getProperty("user.dir") + File.separator + "graph-db";
 
-    GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(new File(graphDbPath)).newGraphDatabase();
-    registerShutdownHook(graphDb);
+    // Set up the database
+    GraphDatabaseService graphDb = getDatabase();
 
-    System.out.println("Attempting to clear database from last run");
-    try (Transaction ignored = graphDb.beginTx();
-         Result result = graphDb.execute("MATCH (n)\n" +
-           "OPTIONAL MATCH (n)-[r]-()\n" +
-           "DELETE n,r")) {
-      System.out.println(result.resultAsString());
-      ignored.success();
-    }
-
-
+    // Set up the PDF Reader
     PdfReader reader = new PdfReader(pdfFilePath);
 
-    System.out.println("Attempting to populate database");
+    // Parse the page labels from the PDF
+    String[] pageLabels = FixedPdfPageLabels.getPageLabels(reader);
+
+    LOGGER.info("Populating database");
     try (Transaction tx = graphDb.beginTx()) {
-      System.out.println("Creating page nodes");
-      String[] bookPageLabels = FixedPdfPageLabels.getPageLabels(reader);
-      Map<PdfObject, String> pdfPageToBookPageLabel = new HashMap<>(reader.getNumberOfPages());
-      for (int pdfPageNumber = 1; pdfPageNumber <= reader.getNumberOfPages(); pdfPageNumber++) {
-        Node pageNode = graphDb.createNode(PageTypes.Page);
-        pageNode.setProperty(PDF_PAGE_NUMBER, pdfPageNumber);
-        pageNode.setProperty(BOOK_PAGE_LABEL, bookPageLabels[pdfPageNumber - 1]);
-        pdfPageToBookPageLabel.put(reader.getPageN(pdfPageNumber), bookPageLabels[pdfPageNumber - 1]);
-      }
+      // Create nodes in the database for all the pages in the PDF
+      Map<PdfObject, String> pdfPageToPageLabel = createPageNodes(reader, pageLabels, graphDb);
 
-      System.out.println("Looping over pages");
-      PdfReaderContentParser contentParser = new PdfReaderContentParser(reader);
-      Map<String, PdfObject> linkDestinations = reader.getNamedDestinationFromStrings();
+      // Process each page, classifying it and updating the database
+      processPages(reader, pageLabels, pdfPageToPageLabel, graphDb);
 
-      for (int pdfPageNumber = 1; pdfPageNumber <= reader.getNumberOfPages(); pdfPageNumber++) {
-//      int pdfPageNumber = 433;
-//      {
-        Node thisPage = graphDb.findNode(PageTypes.Page, PDF_PAGE_NUMBER, pdfPageNumber);
-        if (thisPage == null) {
-          throw new RuntimeException("Couldn't find a node for PDF page: " + pdfPageNumber);
-        }
-
-        String thisBookPageLabel = (String)thisPage.getProperty(BOOK_PAGE_LABEL);
-        boolean isSubBook = false;
-        if (thisBookPageLabel.startsWith("G")) {
-          // All of the sub book (The Murder of Gonzago) page labels start with G
-          thisPage.addLabel(PageTypes.SubBook);
-        }
-        if (!thisBookPageLabel.matches(".*\\d+.*")) {
-          // If there's no digits in the label, ignore the page
-          thisPage.addLabel(PageTypes.Ignore);
-        }
-
-
-        // Check for inter-page-links
-        PdfDictionary pageDict = reader.getPageN(pdfPageNumber);
-        PdfArray annotArray = pageDict.getAsArray(PdfName.ANNOTS);
-        boolean linksOut = false;
-        if (annotArray != null) {
-          for (int i = 0; i < annotArray.size(); i++) {
-            PdfDictionary annotDict = annotArray.getAsDict(i);
-            if (PdfName.LINK == annotDict.get(PdfName.SUBTYPE)) {
-              if (annotDict.contains(PdfName.A)) {
-                String lDestination = ((PdfDictionary) reader.getPdfObject(annotDict.getAsIndirectObject(PdfName.A))).getAsString(PdfName.D).toString();
-                if (linkDestinations.containsKey(lDestination)) {
-                  PdfArray destinationInfoArray = (PdfArray) linkDestinations.get(lDestination);
-                  PdfIndirectReference destinationReference = destinationInfoArray.getAsIndirectObject(0); // TODO, this could actually be an integer for the case of Remote Destinations
-                  PdfObject targetPdfPage = PdfReader.getPdfObject(destinationReference);
-
-                  // Create link if it hasn't already been made between these pages (Split-line links mean two annots with the same dest)
-                  Node targetPage = graphDb.findNode(PageTypes.Page, BOOK_PAGE_LABEL, pdfPageToBookPageLabel.get(targetPdfPage));
-                  boolean existingRelationship = false;
-                  for (Relationship r : thisPage.getRelationships(Direction.BOTH, RelationshipTypes.Choice)) {
-                    existingRelationship |= r.getEndNode().getId() == targetPage.getId();
-                  }
-                  if (!existingRelationship) {
-                    thisPage.createRelationshipTo(targetPage, RelationshipTypes.Choice);
-                    linksOut = true;
-                  }
-                }
-                else {
-                  System.out.println("Found link to unknown: " + lDestination);
-                }
-              }
-              else {
-                System.out.println("Unhandled");
-              }
-            }
-          }
-        }
-
-
-        // Do some context scanning to find page types
-        FontGroupingTextExtractionStrategy strategy = contentParser.processContent(pdfPageNumber, new FontGroupingTextExtractionStrategy(false));
-        if (strategy.getImageCount() > 0 && (strategy.getTextValues().size() == 0 || (strategy.getTextValues().size() == 1 && strategy.getTextValues().get(0).toString().equals(bookPageLabels[pdfPageNumber])))) {
-          // If the page is nothing but an image, label it as an image page (Typically an ending comic)
-          thisPage.addLabel(PageTypes.ImagePage);
-        }
-        else {
-          // If not an image page perhaps there's text to parse?
-          for (StringBuilder textBlock : strategy.getTextValues()) {
-            if (textBlock.toString().matches("THE END(!!)?")) {
-              thisPage.addLabel(PageTypes.EndPage);
-            }
-          }
-        }
-
-        // Link to next page if no other relationships from this node
-        if (!linksOut && !thisPage.hasLabel(PageTypes.EndPage)) {
-          Node nextPage = graphDb.findNode(PageTypes.Page, PDF_PAGE_NUMBER, pdfPageNumber+1);
-          if (nextPage != null) {
-            thisPage.createRelationshipTo(nextPage, RelationshipTypes.Continues);
-          }
-        }
-      }
       tx.success();
     }
+    LOGGER.info("Finished populating database");
 
 
     reader.close();
     graphDb.shutdown();
 
-    // Query e.g. MATCH r=(s:SubBook {book_page_label: "Gi"})-[*..10]->(e:SubBook :EndPage) RETURN r
+    // Query e.g. MATCH r=(s:SubBook)-[*..20]->(e:SubBook :EndPage) RETURN r
   }
 
+  /**
+   * Set up a GraphDatabaseService for Adventurer to fill with data
+   * @return
+   */
+  private static GraphDatabaseService getDatabase() {
+    String graphDbPath = System.getProperty("user.dir") + File.separator + "graph-db";
+
+    GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(new File(graphDbPath)).newGraphDatabase();
+    registerShutdownHook(graphDb);
+
+    clearGraphDB(graphDb);
+
+    return graphDb;
+  }
+
+  /**
+   * Registers a shutdown hook for the Neo4j instance so that it shuts down nicely when the VM exits (even if you
+   * "Ctrl-C" the running application)
+   *
+   * @param graphDb Database to clear
+   */
   private static void registerShutdownHook(final GraphDatabaseService graphDb) {
-    // Registers a shutdown hook for the Neo4j instance so that it
-    // shuts down nicely when the VM exits (even if you "Ctrl-C" the
-    // running application).
+    //
     Runtime.getRuntime().addShutdownHook(new Thread() {
       @Override
       public void run() {
         graphDb.shutdown();
       }
     });
+  }
+
+  /**
+   * Clear out all nodes and relationships from the Graph Database
+   *
+   * @param graphDb Database to clear
+   */
+  private static void clearGraphDB(GraphDatabaseService graphDb) {
+    LOGGER.info("Attempting to clear database from last run");
+    try (Transaction tx = graphDb.beginTx();
+         Result result = graphDb.execute(
+           "MATCH (n)\n" +
+           "OPTIONAL MATCH (n)-[r]-()\n" +
+           "DELETE n, r")
+    ) {
+      // Output the result of the delete statement
+      LOGGER.trace(result.resultAsString());
+
+      tx.success();
+    }
+  }
+
+  /**
+   * First pass of the PDF pages to create unconnected nodes in the database and generate a map of pages to page labels.
+   *
+   * @param reader PdfReader to get PDF information from
+   * @param pageLabels Array of page labels
+   * @param graphDb Database
+   * @return Map of PdfObjects representing pages to page labels
+   */
+  private static Map<PdfObject, String> createPageNodes(PdfReader reader, String[] pageLabels, GraphDatabaseService graphDb) {
+    LOGGER.info("Creating page nodes");
+
+    Map<PdfObject, String> pdfPageToPageLabel = new HashMap<>(reader.getNumberOfPages());
+
+    for (int pdfPageNumber = 1; pdfPageNumber <= reader.getNumberOfPages(); pdfPageNumber++) {
+      Node pageNode = graphDb.createNode(PageTypes.Page);
+      pageNode.setProperty(PDF_PAGE_NUMBER, pdfPageNumber);
+      pageNode.setProperty(BOOK_PAGE_LABEL, pageLabels[pdfPageNumber - 1]);
+      pdfPageToPageLabel.put(reader.getPageN(pdfPageNumber), pageLabels[pdfPageNumber - 1]);
+    }
+
+    LOGGER.info("Finished creating page nodes");
+
+    return pdfPageToPageLabel;
+  }
+
+  /**
+   *
+   * @param reader PdfReader to get PDF information from
+   * @param pageLabels Array of page labels
+   * @param pdfPageToPageLabel Map of PdfObjects representing pages to page labels
+   * @param graphDb Database
+   * @throws IOException
+   */
+  private static void processPages(PdfReader reader, String[] pageLabels, Map<PdfObject, String> pdfPageToPageLabel, GraphDatabaseService graphDb) throws IOException {
+    LOGGER.info("Processing all pages");
+
+    PdfReaderContentParser contentParser = new PdfReaderContentParser(reader);
+
+    // Get a map of all link names to PdfObjects representing pages for the book
+    Map<String, PdfObject> linkDestinations = reader.getNamedDestinationFromStrings();
+
+    // Go through each page again but this time classify them and create links
+    for (int pdfPageNumber = 1; pdfPageNumber <= reader.getNumberOfPages(); pdfPageNumber++) {
+      LOGGER.trace("PDF Page " + pdfPageNumber);
+
+      // Get the pre-existing node from the database for this page
+      Node thisPage = graphDb.findNode(PageTypes.Page, PDF_PAGE_NUMBER, pdfPageNumber);
+      if (thisPage == null) {
+        throw new RuntimeException("Couldn't find a node for PDF page: " + pdfPageNumber);
+      }
+
+      // Get the page label for this page
+      String thisBookPageLabel = (String)thisPage.getProperty(BOOK_PAGE_LABEL);
+
+      // Start classifying the page based on the label
+      if (thisBookPageLabel.startsWith("G")) {
+        // All of the sub book (The Murder of Gonzago) page labels start with G
+        thisPage.addLabel(PageTypes.SubBook);
+      }
+      if (!thisBookPageLabel.matches(".*\\d+.*")) {
+        // If there's no digits in the label, ignore the page (roman numeral pages)
+        thisPage.addLabel(PageTypes.Ignore);
+      }
+
+      // Check for inter-page-links
+      PdfDictionary pageDict = reader.getPageN(pdfPageNumber);
+      PdfArray annotArray = pageDict.getAsArray(PdfName.ANNOTS);
+      boolean linksOut = false;
+      if (annotArray != null) {
+        for (int i = 0; i < annotArray.size(); i++) {
+          PdfDictionary annotDict = annotArray.getAsDict(i);
+          if (PdfName.LINK == annotDict.get(PdfName.SUBTYPE)) {
+            if (annotDict.contains(PdfName.A)) {
+              String lDestination = ((PdfDictionary) reader.getPdfObject(annotDict.getAsIndirectObject(PdfName.A))).getAsString(PdfName.D).toString();
+              if (linkDestinations.containsKey(lDestination)) {
+                PdfArray destinationInfoArray = (PdfArray) linkDestinations.get(lDestination);
+                PdfIndirectReference destinationReference = destinationInfoArray.getAsIndirectObject(0); // TODO, this could actually be an integer for the case of Remote Destinations
+                PdfObject targetPdfPage = PdfReader.getPdfObject(destinationReference);
+
+                // Create link if it hasn't already been made between these pages (Split-line links mean two annots with the same dest)
+                Node targetPage = graphDb.findNode(PageTypes.Page, BOOK_PAGE_LABEL, pdfPageToPageLabel.get(targetPdfPage));
+                boolean existingRelationship = false;
+                for (Relationship r : thisPage.getRelationships(Direction.BOTH, RelationshipTypes.Choice)) {
+                  existingRelationship |= r.getEndNode().getId() == targetPage.getId();
+                }
+                if (!existingRelationship) {
+                  thisPage.createRelationshipTo(targetPage, RelationshipTypes.Choice);
+                  linksOut = true;
+                }
+              }
+              else {
+                LOGGER.warn("Found link to unknown: " + lDestination);
+              }
+            }
+            else {
+              LOGGER.warn("Adventurer only handles Anchor Links");
+            }
+          }
+        }
+      }
+
+
+      // Do some context scanning to find page types
+      FontGroupingTextExtractionStrategy strategy = contentParser.processContent(pdfPageNumber, new FontGroupingTextExtractionStrategy(false));
+      if (strategy.getImageCount() > 0 && (strategy.getTextValues().size() == 0 || (strategy.getTextValues().size() == 1 && strategy.getTextValues().get(0).toString().equals(pageLabels[pdfPageNumber])))) {
+        // If the page is nothing but an image, label it as an image page (Typically an ending comic)
+        thisPage.addLabel(PageTypes.ImagePage);
+      }
+      else {
+        // If not an image page perhaps there's text to parse?
+        for (StringBuilder textBlock : strategy.getTextValues()) {
+          if (textBlock.toString().matches("THE END(!!)?")) {
+            thisPage.addLabel(PageTypes.EndPage);
+          }
+        }
+
+        String[] words = strategy.getResultantText().split("\\s+");
+        thisPage.setProperty(WORD_COUNT, words.length);
+      }
+
+      // Link to next page if no other relationships from this node
+      if (!linksOut && !thisPage.hasLabel(PageTypes.EndPage)) {
+        Node nextPage = graphDb.findNode(PageTypes.Page, PDF_PAGE_NUMBER, pdfPageNumber+1);
+        if (nextPage != null) {
+          thisPage.createRelationshipTo(nextPage, RelationshipTypes.Continues);
+        }
+      }
+    }
+
+    LOGGER.info("Finished processing all pages");
   }
 }
